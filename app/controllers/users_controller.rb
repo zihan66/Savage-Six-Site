22f51a31cb114f5a91e5dc9194d6a6127d0b3e31
Zihan Wei <zihan66@tamu.edu>
2019-02-22 17:17:20 -0600

    Sign up page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
-  def new
-  end
-
-
+def user_params
+  params.require(:user).permit(:name, :email, :password,
+                               :password_confirmation)
+end
+def show
+  @user = User.find(params[:id])
+  debugger
+end
+def new
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
-  root 'users#new'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
+  # ...
 end
