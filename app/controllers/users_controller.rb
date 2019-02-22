ad48230254d9a0d8e2331d953ab5e63c2861d3f4
jhamilton248 <jhamilton248@tamu.edu>
2019-02-22 13:48:43 -0600

    Filled out new user form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,7 @@ class UsersController < ApplicationController
   def new
   end
 
+ def index
 
+ end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/new'
 
-  root 'users#new'
+
+  root 'users#index'
+
+   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
