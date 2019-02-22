0219e03b289f4df5603199d0e932ccb748e1cf58
Zihan Wei <zihan66@tamu.edu>
2019-02-22 12:46:18 -0600

    erb

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,12 @@
+class UsersController < ApplicationController
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+  end
+  def show
+    @user = User.find(params[:id])
+    debugger
+  end
+  def new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  resources :users
+  get 'users/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  Rails.application.routes.draw do
+    root 'static_pages#home'
+    get  '/help',    to: 'static_pages#help'
+    get  '/about',   to: 'static_pages#about'
+    get  '/contact', to: 'static_pages#contact'
+    get  '/signup',  to: 'users#new'
+    resources :users
+  end
+  #Adding a Users resource to the routes file.
 end
