cbeaf3a82b0e1481addbb2f40c76a05b8862b60f
jhamilton248 <jhamilton248@tamu.edu>
2019-03-05 11:41:21 -0600

    Added log out functionality; linked log in and log out on landing page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      login @user
       flash[:success] = "Welcome to Savage Six Site!"
       redirect_to @user
     else
