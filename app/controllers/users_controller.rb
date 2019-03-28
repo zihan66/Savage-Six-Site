c7d349d0c8e7f432b289f65b1130d953b32413ba
jhamilton248 <jhamilton248@tamu.edu>
2019-03-28 12:22:39 -0500

    added redirects to edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,56 +1,64 @@
 class UsersController < ApplicationController
-  def index
-  end
+   def index
+   end
 
-  def show
-    if !logged_in?
-      redirect_to login_path
-      flash[:alert] = "You must be logged in to view profiles.  "
-    else
-      @user = User.find(params[:id])
-    end
-  end
+   def show
+      if !logged_in?
+         redirect_to login_path
+         flash[:alert] = "You must be logged in to view profiles.  "
+      else
+         @user = User.find(params[:id])
+      end
+   end
 
-  def new
-    @user = User.new
-  end
+   def new
+      @user = User.new
+   end
 
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      log_in @user
-      flash[:notice] = "Welcome to Savage Six Site!"
-      redirect_to @user
-    else
-      flash[:alert] = @user.errors.full_messages.join("<br/>").html_safe
-      render 'new'
-    end
-  end
+   def create
+      @user = User.new(user_params)
+      if @user.save
+         log_in @user
+         flash[:notice] = "Welcome to Savage Six Site!"
+         redirect_to @user
+      else
+         flash.now[:alert] = @user.errors.full_messages.join("<br/>").html_safe
+         render 'new'
+      end
+   end
 
-  def edit
-    @user = User.find(params[:id])
-    #if @user.update_attributes(user_params)
-     # flash[:success] = "Profile updated"
-     # redirect_to @user
-    #else
-      #render 'edit'
-    #end
-    end
+   def edit
+      if !logged_in?
+         redirect_to login_path and return
+      elsif !is_user(params[:id])
+         redirect_to current_user and return
+      end
 
-  def update
-    @user = User.find(params[:id])
-    if @user.update_attributes(user_params)
+   @user = User.find(params[:id])
+   #if @user.update_attributes(user_params)
+   # flash[:success] = "Profile updated"
+   # redirect_to @user
+   #else
+   #render 'edit'
+   #end
+end
+
+def update
+   @user = User.find(params[:id])
+   if @user.update_attributes(user_params)
       flash[:success] = "Profile updated"
       redirect_to @user
-    else
+   else
       render 'edit'
-    end
-  end
+   end
+end
 
-  private
-  def user_params
-     params.require(:user).permit(:FirstName, :lastname, :email, :password,
-                                     :password_confirmation, :major, :classyear, :occupation, :phonenumber, :city,
-                                      :state, :country)
-  end
+private
+
+def user_params
+   params.require(:user).permit(:FirstName, :lastname, :email, :password,
+                                :password_confirmation, :major, :classyear, :occupation, :phonenumber, :city,
+                                :state, :country)
+end
+
 end
