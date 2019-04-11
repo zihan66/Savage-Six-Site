4be31b7ef2be611198d5ba58e2b05863dfcad742
Zihan Wei <zihan66@tamu.edu>
2019-04-11 11:30:25 -0500

    precompile

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,7 @@ require File.expand_path('../boot', __FILE__)
 Bundler.require(*Rails.groups)
 module SavageSixSite
   class Application < Rails::Application
+    config.assets.initialize_on_precompile = false
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     # Settings in config/environments/* take precedence over those specified here.
