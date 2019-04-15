5ab012bf204066aac2e1a41dd5b005ed6164d6b9
Zihan Wei <zihan66@tamu.edu>
2019-04-14 20:53:52 -0500

    test

--- db/migrate/20190414221238_change_year_to_be_int_int_users.rb
+++ db/migrate/20190414221238_change_year_to_be_int_int_users.rb
@@ -1,7 +1,6 @@
 class ChangeYearToBeIntIntUsers < ActiveRecord::Migration[5.2]
   def change
-    User.where(classyear:'{1=>2019, 2=>2, 3=>1}').update_all({classyear: 0})
-    change_column :users, :classyear,  using: 'classyear::integer'
+    change_column :users, :classyear, :integer, using: 'classyear::integer'
     change_column_default :users, :classyear, 0
     change_column_null :users, :classyear, 0
   end
