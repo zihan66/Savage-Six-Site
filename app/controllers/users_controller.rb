d1d4aa7e40d4c45288064423b0844f5d6606dee1
Zihan Wei <zihan66@tamu.edu>
2019-04-16 12:37:42 -0500

    fix admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,11 @@ class UsersController < ApplicationController
       end
       @users = User.search(params[:search], params[:filter])
 
+      byebug
+
       if @users.length == 0
          flash.now.alert = "No users found."
-         @users = User.where("admin = ?", false)
+         @users = User.where(admin: false)
       end
    end
 
